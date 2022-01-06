97fd0920e60ff23b97806a8745b644be903b9a73
berna39 <kalemajoseph39@gmail.com>
2022-01-06 13:28:23 +0200

    Varables

--- python_basics/variables.py
+++ python_basics/variables.py
@@ -1,1 +1,23 @@
 #this is my first step toward my dreams
+x  = "Hello World" #string value
+print(x)
+print(type(x))
+
+y= 12
+print(y)
+
+s = "Tobirama senji"
+S = "Azuma sarutobi"
+print(s)
+
+my_variable = "Hitachi"
+print("Hello "+my_variable)
+
+first, second, third = "Hashirama", "Tobirama", "Sarutobi"
+print(first)
+print(second)
+print(third)
+
+my_contry = my_home = "Kenya"
+print(my_home)
+print(my_contry)
