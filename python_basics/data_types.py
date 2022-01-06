0598d4607dac8231e440cb0bbaf01396a7196038
berna39 <kalemajoseph39@gmail.com>
2022-01-06 13:37:19 +0200

    Data Types

---
+++ python_basics/data_types.py
@@ -0,0 +1,11 @@
+#data types
+b = "Naruto" # string
+c = 12 #int
+d = 10.3 #float
+e = 1j #complex number
+f = [ "Kakashi", "Obito", "Rhin" ] #list
+g = ( "Tsunade", "Horoshimaru", "Jiraya" ) #tuples
+h = range(39) #range
+i = { "team": "redbull", "pilot": "Verstapen" } #dictionary
+j = { "Suna", "Kiri", "Konoa" } #set
+
