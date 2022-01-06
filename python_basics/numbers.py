54889e48e80dacfeb3f46c298cfc04ba41f97800
berna39 <kalemajoseph39@gmail.com>
2022-01-06 13:45:41 +0200

    Numbers

---
+++ python_basics/numbers.py
@@ -0,0 +1,29 @@
+# Numbers
+a = 3
+b = 3.14
+k = 1j
+
+other = .34 #float value
+
+print(type(a))
+print(type(b))
+print(type(k))
+
+
+x = 23
+y = 234908239048230
+z = -23e9
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+normal = 5
+complex_number = 2j
+print(normal+complex_number)
+
+converted = complex(normal);
+
+print(float(normal))
+print(converted)
+
