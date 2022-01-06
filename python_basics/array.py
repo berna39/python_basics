b88057f51c2e14d68b2210b21b9c2449df100655
berna39 <kalemajoseph39@gmail.com>
2022-01-06 14:22:46 +0200

    Arrays

---
+++ python_basics/array.py
@@ -0,0 +1,20 @@
+#array
+drinks = ['pespi', 'cola', 'redbull']
+print(drinks[0])
+
+#array length
+print(len(drinks))
+
+#adding sth to the list
+drinks.append("fanta")
+print(drinks[3])
+
+#removing values
+drinks.pop(1) #removing cola
+print(drinks)
+
+drinks.remove("fanta") #removing fanta
+print(drinks)
+
+
+
