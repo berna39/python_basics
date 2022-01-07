8abb19d0bc7924b7b301737f27b33bb0df64fea3
berna39 <kalemajoseph39@gmail.com>
2022-01-07 13:57:13 +0200

    Functions anvanced

--- python_basics/function.py
+++ python_basics/function.py
@@ -4,7 +4,19 @@ def sayHello():
 def addition(a, b):
     return a + b
 
+#if we don't how much parameters will be passed
+def buy_cars(*cars):
+    print('i buy '+cars[2])
+
+#default parameter value
+def say_country(country = 'Kenya'):
+    print('My country is '+country)
+
 sayHello()
 
 sum = addition(12, 8)
 print(str(sum))
+
+buy_cars('volvo', 'toyota', 'honda', 'mercedes')
+
+say_country('DRC')
