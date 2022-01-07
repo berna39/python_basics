20b051ca8c2796e83e9736cbd37dc2957207d5cb
berna39 <kalemajoseph39@gmail.com>
2022-01-07 12:13:22 +0200

    Tuples

---
+++ python_basics/tuples.py
@@ -0,0 +1,9 @@
+#used to store multiple elements into one variable
+my_tuple = ('kakashi', 'obito', 'rhin')
+print(my_tuple)
+
+my_other_tuple = ('toby', 'kabuto', 39, True)
+print(my_other_tuple)
+
+anoter_way_tupple = tuple(('code', 'pray', 'sleep', 'play'))
+print(anoter_way_tupple)
