e729b8decc35e99d4e9fd0fd7d051eb353369eb5
berna39 <kalemajoseph39@gmail.com>
2022-01-07 12:28:47 +0200

    End of Tuples

--- python_basics/tuples.py
+++ python_basics/tuples.py
@@ -7,3 +7,29 @@ print(my_other_tuple)
 
 anoter_way_tupple = tuple(('code', 'pray', 'sleep', 'play'))
 print(anoter_way_tupple)
+
+
+#operations
+testing_tuple = ('hashirama', 'tobirama', 'sarutoni', 'minato', 'tsunade', 'kakashi', 'naruto')
+
+print(testing_tuple[1]) # access item
+print(testing_tuple[-3]) # access item with reverse order
+print(testing_tuple[1:4]) #range indexing
+print(testing_tuple[-4:-1]) #range indexing with negative numbers
+if 'kakashi' in testing_tuple: #testing if a tuple conains any value
+    print('hello Sensei')
+
+
+#tuple's values are inchangeable but we can convert it into a list and then convert it back to a tuple
+my_list = list(testing_tuple)
+my_list[0] = 'madara'
+testing_tuple = tuple(my_list)
+print(testing_tuple)
+
+#adding sth to a tuple
+my_list = list(testing_tuple)
+my_list.append('terminator')
+testing_tuple = tuple(my_list)
+print(testing_tuple)
+
+
