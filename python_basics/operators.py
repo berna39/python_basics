c38fea49f7d49fd59cee156954ce98ba3487ecd8
berna39 <kalemajoseph39@gmail.com>
2022-01-07 11:57:25 +0200

    Basics operators

---
+++ python_basics/operators.py
@@ -0,0 +1,23 @@
+#Arithmetic operations
+x = 21
+y = 7
+
+print(x+y) #addition
+print(x-y) #substraction
+print(x*y) #multiplication
+print(x/y) #division
+print(x**y) #addition
+print(x//y) #Floor division
+
+#assignment operator
+number = 0
+
+number += 5 #same as number = number + 5
+number -= 5 #same as number = number - 5
+number *= 5 #same as number = number * 5
+number **= 5 #same as number = number ** 5
+number /= 5 #same as number = number / 5
+number //= 5 #same as number = number // 5
+
+
+
