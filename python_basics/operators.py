3c535ddcf6c719c14b1f5e17a252cf01647c87c1
berna39 <kalemajoseph39@gmail.com>
2022-01-07 12:05:19 +0200

    End of Operators

--- python_basics/operators.py
+++ python_basics/operators.py
@@ -19,5 +19,34 @@ number **= 5 #same as number = number ** 5
 number /= 5 #same as number = number / 5
 number //= 5 #same as number = number // 5
 
+#logical operations
+age = 18
+gander = 'm'
+
+if age >= 18 and gander == 'm':
+    print("hello adult man")
+
+
+country = 'Belgium'
+if country == 'DRC' or country == 'Kenya':
+    print('I\'m home')
+else:
+    print('Hello stranger')
+
+
+o = "Stromae"
+p = o
+q = "Stromae"
+
+if o is p:
+    print('o is p')
+
+if o is q:
+    print('o is q')
+else:
+    print('o is not q')
+
+
+
 
 
