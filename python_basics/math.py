063f95c3914baf9ac349fda6dd43cc7d9cc6ed73
berna39 <kalemajoseph39@gmail.com>
2022-01-07 13:45:00 +0200

    Basic maths operation

---
+++ python_basics/math.py
@@ -0,0 +1,15 @@
+import math
+
+loweer = min(2,56,7,8,9,3,23,7,-2)
+print('The lower is :' + str(loweer))
+
+greater = max(3,5,634,6,3,8,34,-5)
+print('The grater is :' + str(greater))
+
+x = abs(-5.45)
+print(x)
+
+s = 2
+print(pow(s, 3)) #same as s * s * s
+
+print(math.sqrt(64))
