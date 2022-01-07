458e7ec78fd83d3b68dc1fc86c0b7e0d561725cd
berna39 <kalemajoseph39@gmail.com>
2022-01-07 20:10:52 +0200

    Dictionary basics

---
+++ python_basics/dictionary.py
@@ -0,0 +1,11 @@
+my_first_dic = {
+    "name": "kalema shango joseph",
+    "age": 23,
+    "is_christian": True,
+    "hobies": [
+        { "name": "coding", "is_funny": True },
+        { "name": "soccer", "is_funny": True }
+    ]
+}
+
+print(my_first_dic)
