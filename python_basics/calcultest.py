0440f45fcf1efa78168a978bb646e4b62abc203a
berna39 <kalemajoseph39@gmail.com>
2022-01-08 23:49:06 +0200

    Basic game

---
+++ python_basics/calcultest.py
@@ -0,0 +1,10 @@
+print('welcome to the world of game \n')
+name = input('enter your name please ')
+
+print('Hello dear '+ name)
+print('------------------ \n')
+
+first_number = input('enter the first number' )
+second_number = input('enter the second number' )
+sign = input('choose a sign : +, -, * , /')
+
