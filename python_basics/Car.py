3763fe0c93c9bf7fb9eaac8358ecc7fe65c0c106
berna39 <kalemajoseph39@gmail.com>
2022-01-08 23:59:23 +0200

    evolving with OOP again

---
+++ python_basics/Car.py
@@ -0,0 +1,13 @@
+class Car:
+    mark = ''
+    model = ''
+
+    def __init__(self, mark, model):
+        self.mark
+        self.model
+    
+    def drive(self):
+        print('i drive ' + self.model +' - '+ self.mark)
+    
+    def vruum(selft):
+        print('vrouuuuum')
