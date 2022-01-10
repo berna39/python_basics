e802e9f76bf00a96b8d0fd03e81eb489fd2b5083
berna39 <kalemajoseph39@gmail.com>
2022-01-10 21:35:55 +0200

    Dealing with sets

---
+++ python_basics/sets.py
@@ -0,0 +1,18 @@
+#defining a set
+first_set = {'suna', 'kiri', 'konoa', 'iwa'}
+
+print(first_set)
+
+#a set length
+print(len(first_set))
+
+#test if a set contains sth
+print('kiri' in first_set)
+
+#remove element from kiri
+first_set.remove('suna')
+
+#adding element to a set
+first_set.add('kumo')
+print(first_set)
+
