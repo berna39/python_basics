0e8271cb62c82c9e6a0b9254c532fcaad314c7e9
berna39 <kalemajoseph39@gmail.com>
2022-01-11 09:08:08 +0200

    More operations on regex

--- python_basics/regex.py
+++ python_basics/regex.py
@@ -4,3 +4,25 @@ text = 'Hakuna matata Kenya'
 #test if my text starts by Hakuna and ends by Kenya and return a matchobject
 x = re.search('^Hakuna.*Kenya$', text)
 print(x)
+
+
+# the [] is for a set of characters
+other = 'Piaga kazi'
+y = re.search('[x-z]', other)
+print(y)
+
+#the {} Exactly the specified number of occurrences
+my_name = 'Shango'
+print(re.search('Sha{2}go', my_name))
+
+#the ^ represent starts with
+band = 'Sauti sol'
+print(re.search('^Sauti', band))
+
+#the $ represent ends with
+band = 'Sauti sol'
+print(re.search('sol$', band))
+
+#oops ! care about the caracter case
+band = 'Sauti sol'
+print(re.search('Sol$', band))
