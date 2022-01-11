ca016ac5acbbdee7cac5db2bf43c7b03a7ae0a1a
berna39 <kalemajoseph39@gmail.com>
2022-01-11 12:16:28 +0200

    More about File handling

--- python_basics/file_handling.py
+++ python_basics/file_handling.py
@@ -1,2 +1,10 @@
-t = open("testing.txt", "rt")
+# readline allows us to read only one line
+t = open("testing.txt", "rt").readline()
 print(t)
+
+#Because "r" for read, and "t" for text are the default values, you do not need to specify them.
+u = open('testing.txt', 'rt').read()
+print(u)
+
+
+
