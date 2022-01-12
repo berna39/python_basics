c821c03d820a67026f7e429a0a12f18f507c7170
berna39 <kalemajoseph39@gmail.com>
2022-01-12 23:49:37 +0200

    Iterator on a srt value

--- python_basics/iterators.py
+++ python_basics/iterators.py
@@ -5,3 +5,12 @@ myit = iter(my_tuple)
 print(next(myit))
 print(next(myit))
 print(next(myit))
+
+my_teacher = 'Kylie'
+
+my_teacher_iterator = iter(my_teacher)
+print(next(my_teacher_iterator))
+print(next(my_teacher_iterator))
+print(next(my_teacher_iterator))
+print(next(my_teacher_iterator))
+print(next(my_teacher_iterator))
