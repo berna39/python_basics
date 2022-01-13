16eda6adbed1278f09767b1bb6f653bfa7cb574e
berna39 <kalemajoseph39@gmail.com>
2022-01-13 23:45:39 +0200

    Firt future of my little game

--- python_basics/little_game.py
+++ python_basics/little_game.py
@@ -1,3 +1,8 @@
+import string
+
 print("welcom to the game worl")
 name = input('enter your name : ')
 print('Hello '+name+', choose option')
+
+def play():
+    all_letters = string.ascii_uppercase
