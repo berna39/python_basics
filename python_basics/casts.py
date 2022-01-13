47ae5eaf15b424bdb6fd03cc43c3ffb6e87ca05c
berna39 <kalemajoseph39@gmail.com>
2022-01-13 23:49:21 +0200

    Tuple casting to list

--- python_basics/casts.py
+++ python_basics/casts.py
@@ -1,5 +1,8 @@
 #casting types
 
+from typing import Literal
+
+
 a  = 12
 float_casted = float(a)
 print(float_casted)
@@ -7,3 +10,7 @@ print(float_casted)
 b = 6
 string_casted = str(b)
 print(type(string_casted))
+
+my_tuple = ('tom', 'alice')
+list_squad = list(my_tuple)
+print( 'the squad is : '+ ' & '.join(list_squad))
