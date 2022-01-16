fc004a55fe644e5b273a12362993da4c783b7d54
berna39 <kalemajoseph39@gmail.com>
2022-01-16 22:32:57 +0200

    Python unit testing setup data

--- python_unit_testing/testpanda.py
+++ python_unit_testing/testpanda.py
@@ -2,13 +2,14 @@ import unittest
 from panda import Panda
 
 class TestPanda(unittest.TestCase):
+    def setUp(self):
+        self.po = Panda('Poo', 12)
+
     def test_panda_is_instance_of_panda(self):
-        po = Panda('Poo', 12)
-        self.assertIsInstance(po, Panda)
+        self.assertIsInstance(self.po, Panda)
 
     def test_panda_age_is_positive(self):
-        po = Panda('Poo', 12)
-        self.assertGreater(po.age, 0)
+        self.assertGreater(self.po, 0)
 
 if __name__ == '__main__':
     unittest.main()
