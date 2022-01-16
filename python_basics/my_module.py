df69f7c5847874f00169a639c45b9cafe72eef0b
berna39 <kalemajoseph39@gmail.com>
2022-01-16 23:33:10 +0200

    Adding a class to a module

--- python_basics/my_module.py
+++ python_basics/my_module.py
@@ -1,6 +1,16 @@
 #this is a function defined to module
+from mimetypes import init
+
+
 def sayHello(name):
     print('Hello ' + name)
 
 def addition(number_one, number_two):
     print('the dum of ' + str(number_one) + ' and ' + str(number_two) + ' is ' + str(number_one + number_two))
+
+class Movie:
+    def __init__(self, title):
+        self.title = title
+    
+    def __str__(self) -> str:
+        return f'title : {self.title}'
