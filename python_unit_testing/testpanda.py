29d1783633c71aa2f2eb04fd7ae292e75be42c1f
berna39 <kalemajoseph39@gmail.com>
2022-01-16 22:41:04 +0200

    More about changing testing data

--- python_unit_testing/testpanda.py
+++ python_unit_testing/testpanda.py
@@ -5,11 +5,15 @@ class TestPanda(unittest.TestCase):
     def setUp(self):
         self.po = Panda('Poo', 12)
 
+    def tearDown(self):
+        pass
+
     def test_panda_is_instance_of_panda(self):
         self.assertIsInstance(self.po, Panda)
 
     def test_panda_age_is_positive(self):
-        self.assertGreater(self.po, 0)
+        print(self.po)
+        self.assertGreater(self.po.age, 0)
 
 if __name__ == '__main__':
     unittest.main()
