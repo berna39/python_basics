32bf1c94d3c9d5416bf7cfb920c4be8b425caf06
berna39 <kalemajoseph39@gmail.com>
2022-01-18 23:58:41 +0200

    Some False values

--- python_basics/booleans.py
+++ python_basics/booleans.py
@@ -3,3 +3,11 @@ print(10 == 9) #True
 
 print(bool('hello')) #any string is true except the empty one
 print(bool(15)) #any number is true except 0
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
