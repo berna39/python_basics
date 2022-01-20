4ba976dd3cbf3c336cd4b92113c9e51487096620
berna39 <kalemajoseph39@gmail.com>
2022-01-20 20:18:33 +0200

    More about functions

--- python_basics/function.py
+++ python_basics/function.py
@@ -20,3 +20,26 @@ print(str(sum))
 buy_cars('volvo', 'toyota', 'honda', 'mercedes')
 
 say_country('DRC')
+
+#advanced topic about functions
+#python's functions are first class citizen
+
+def plus_one(number):
+    return number + 1
+
+increment = plus_one #assigning a function to a variable
+print(increment(2))
+
+#definining a function into another
+def process(number):
+    print(f'the number passed is {number}')
+
+    def increment(someting):
+        return someting + 1
+    
+    result = increment(number)
+
+    return result
+
+print(process(3))
+
