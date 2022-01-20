c6172459836f8d795afd88b08a7697e7379ebc3d
berna39 <kalemajoseph39@gmail.com>
2022-01-20 22:07:16 +0200

    Basic case

--- python_basics/decorators.py
+++ python_basics/decorators.py
@@ -1,1 +1,15 @@
 #A decorator is a design pattern in Python that allows a user to add new functionality to an existing object without modifying its structure.
+def upper_case_decorator(function):
+    def wrapper():
+        func = function()
+        upper_case_format = func.upper()
+        return upper_case_format
+
+    return wrapper
+
+
+def sayHello():
+    return 'hello world'
+
+upper = upper_case_decorator(sayHello)
+print(upper())
