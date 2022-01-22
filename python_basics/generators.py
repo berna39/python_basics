56a7bf095010509cf574d5f4706a270c66cadbe3
berna39 <kalemajoseph39@gmail.com>
2022-01-22 23:54:20 +0200

    My first generator

--- python_basics/generators.py
+++ python_basics/generators.py
@@ -1,1 +1,12 @@
 #For lazy execution
+
+def my_generator(n):
+    for  x in range(1, n):
+        yield x ** 3
+
+values = my_generator(100)
+
+print(next(values))
+print(next(values))
+print(next(values))
+print(next(values))
