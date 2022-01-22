b8cfa3762ed23ba4e7f97f280c1086a2aaa07ec9
berna39 <kalemajoseph39@gmail.com>
2022-01-22 23:28:32 +0200

    The Del magic method

--- python_basics/magic_methods.py
+++ python_basics/magic_methods.py
@@ -9,5 +9,8 @@ class Team:
     def __str__(self) -> str: #the str magic method for object details as string
         return f'name : {self.name}, country : {self.country}'
 
+    def __del__(self): #this will be called once the object is destroyed
+        print('The object has been destroyed')
+
 team = Team('liecester', 'England')
 print(team)
