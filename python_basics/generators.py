99ed33553b46707a1066b0dc68139d5ab5ff4c72
berna39 <kalemajoseph39@gmail.com>
2022-01-22 23:56:25 +0200

    Combine with iteratos

--- python_basics/generators.py
+++ python_basics/generators.py
@@ -4,9 +4,14 @@ def my_generator(n):
     for  x in range(1, n):
         yield x ** 3
 
-values = my_generator(100)
+values = my_generator(10)
 
 print(next(values))
 print(next(values))
 print(next(values))
 print(next(values))
+
+#getting the next value with iterators:
+
+for x in values:
+    print(x)
