0b7ca8450ee552eee91e467d595af90ecb46d9aa
berna39 <kalemajoseph39@gmail.com>
2022-01-23 20:22:10 +0200

    The concept of encapsulation

---
+++ python_basics/encapsulation.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age, gander):
+        self.__name = name
+        self.__age = age
+        self.__gander = gander
+
+
+
+p = Person('Hashirama senji', 43, 'M')
+#print(p.__age) this won't work
