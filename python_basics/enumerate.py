d47986ea6d420771d72b52bb901a29ed0b50c63e
berna39 <kalemajoseph39@gmail.com>
2022-01-25 23:25:48 +0200

    the problem definition

---
+++ python_basics/enumerate.py
@@ -0,0 +1,11 @@
+values = ['Meschack', 'Shadrack', 'abedNego']
+
+for x in values: #nirmal loop
+    print(x)
+
+#now let try to print also the index of of each element
+
+index = 0 
+for x in values: #nirmal loop
+    print(index, x)
+    index += 1
